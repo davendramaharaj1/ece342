fcff7e2a3a95876f295f45c81a70d222b1c574ba
davendramaharaj1 <davendramaharaj0@gmail.com>
2021-04-18 16:59:42 -0400

    Instantiated cpu module in lab6 part1

--- lab6/part1.sv
+++ lab6/part1.sv
@@ -6,4 +6,28 @@ module part1
     output      [ 7: 0]         LEDR
 );
 
+/* instantiate RISC-V CPU from Lab 5 */
+cpu#(.IW(32), .REGS(32)) cpu(
+
+    .clk(),
+    .reset(),
+
+    // read only port connecting to read only port on 32KB RAM or SW
+    .o_pc_addr(),
+    .o_pc_rd(),
+    .i_pc_rddata(),
+    .o_pc_byte_en(),
+
+    // read/write port connecting to read/write port on 32KB RAM or LED
+    .o_ldst_addr(),
+    .o_ldst_rd(),
+    .o_ldst_wr(),
+    .i_ldst_rddata(),
+    .o_ldst_wrdata(),
+    .o_ldst_byte_en(),
+
+    // testbench reg file --> not used in lab 6 so can safely ignore 
+    .o_tb_regs()
+);
+
 endmodule
